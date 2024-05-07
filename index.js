288c4ea4e2b0c7e4d6499547426f4f4ab32a0563
ewyduhji <gs1sirijorn@hotmail.com>
2024-05-08 05:38:39 +0700

    aptos sdk project

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
-function letterCombinations(digits) {
-  if (digits.length === 0) return [];
-  const map = {
-    2: "abc",
-    3: "def",
-    4: "ghi",
-    5: "jkl",
-    6: "mno",
-    7: "pqrs",
-    8: "tuv",
-    9: "wxyz",
-  };
-  const result = [];
-  backtrack("", digits);
-  return result;
-  function backtrack(combination, nextDigits) {
-    if (nextDigits.length === 0) result.push(combination);
-    else {
-      const digit = nextDigits.substring(0, 1);
-      const letters = map[digit];
-      for (const letter of letters) {
-        backtrack(combination + letter, nextDigits.substring(1));
-      }
-    }
+function rob(nums) {
+  let prevMax = 0;
+  let currMax = 0;
+  for (const num of nums) {
+    const temp = currMax;
+    currMax = Math.max(prevMax + num, currMax);
+    prevMax = temp;
   }
+  return currMax;
 }
