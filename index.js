e048390be09ff4c41293e4b325157dd14273fb3c
ewyduhji <gs1sirijorn@hotmail.com>
2024-05-11 00:22:39 +0700

    aptos

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-function isSymmetric(root) {
-  if (!root) return true;
-  return isMirror(root.left, root.right);
-  function isMirror(left, right) {
-    if (!left && !right) return true;
-    if (!left || !right || left.val !== right.val) return false;
-    return isMirror(left.left, right.right) && isMirror(left.right, right.left);
-  }
+const number = 10;
+if (number % 2 === 0) {
+  console.log("Even number");
+} else {
+  console.log("Odd number");
 }
